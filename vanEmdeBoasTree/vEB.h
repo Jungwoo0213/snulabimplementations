33a1e032c6977e313d760b5cf55cf0e5f46f382b
jungwoo <jwyang0213@gmail.com>
2020-02-20 18:59:04 +0900

    vEB

--- vanEmdeBoasTree/vEB.h
+++ vanEmdeBoasTree/vEB.h
@@ -9,7 +9,7 @@ using namespace std;
 class vEB{
 public:
     int u;
-    
+
 
     vEB(int u){
         if(isPowerOfTwo(u)==false){
@@ -19,9 +19,17 @@ public:
 
 private:
     int high(int x){
-        
+        return floor((double)x/lowerSqrt());
     }
-
+    int low(int x){
+        return x % lowerSqrt();
+    }
+    int index(int x, int y){
+        return x*lowerSqrt() + y;
+    }
+    int lowerSqrt(){
+        return  1 << int(floor(log2((double)u)/2));
+    }
     bool isPowerOfTwo(int x){
         return (x!=0) && ((x & (x-1))==0);
     }
