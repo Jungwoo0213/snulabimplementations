5bb287d984aca3b0202763ffff3904a4c8755f0a
jungwoo <jwyang0213@gmail.com>
2020-03-25 21:38:03 +0900

    debugged heapsort

--- heapsort/heapsort.h
+++ heapsort/heapsort.h
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 
 class HeapSort{
+public:
 	int* A;
 	int size;
+    int len;
 
 	bool MaxHeap;
 
@@ -14,22 +18,57 @@ class HeapSort{
 		return (ind << 1)+2;
 	}
 	
-	HeapSort(bool ifMax = true){
+	HeapSort(int *tA, int size, bool ifMax = true){
+        A = tA;
+        this->size = len = size;
 		MaxHeap = ifMax;
 	}
 	void MaxHeapify(int i){
 		int l = left(i);
 		int r =right(i);
 		int largest = i;
-		if(l<= size && A[l]>A[i])
+		if(l< size && A[l]>A[i])
 			largest = l;
-		if(r<= size && A[r] > A[largest])
+		if(r< size && A[r] > A[largest])
 			largest = r;
 		if (largest != i){
-            int temp = A[i];
-            A[i] = A[largest];
-            A[largest] = temp;
+            swap(i, largest);
             MaxHeapify(largest);
 		}
 	}
+    void buildHeap(){
+        size = len;
+        for( int i=floor(len/2)-1; i>=0; i--)
+            MaxHeapify(i);
+    }
+    void sort(){
+        buildHeap();
+        for(int i=len-1; i>=1; i--){
+            swap(0, i);
+            size--;
+            MaxHeapify(0);
+        }
+    }
+    void print(){
+        for(int i=0; i<len;i++){
+            cout << A[i]<<" ";
+        }
+        cout <<endl;
+    }
+    void check(){
+        int a, b;
+        for(int i=0;i<len-1;i++){
+            a = A[i];
+            b = A[i+1];
+            if(a>b)
+                cout << "Sort is wrong"<<endl;
+        }
+        cout << "Sort is correct!"<<endl;
+    }
+private:
+    inline void swap(int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
 };
--- heapsort/main.cpp
+++ heapsort/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-
+#include <stdlib.h>
 #include "heapsort.h"
 
 using namespace std;
 
+const int size =100;
+int A[size];
+
+
 int main(){
+
+    srand(1);
+    for(int i=0; i<size; i++)
+        A[i] = rand()%100;
+
+    HeapSort hp(A, size);
+    hp.sort();
+    hp.print();
+    hp.check();
 
 	return 0;
 }
